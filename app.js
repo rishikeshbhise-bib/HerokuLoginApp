5400b5129d23080e37576f6603292eed07b411a5
rishikeshbhise-bib <50315322+rishikeshbhise-bib@users.noreply.github.com>
2019-05-08 12:37:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
 res.end('<h1>Apostrophe Inline Login Demo</h1>');
+const express = require('express');
+const app = new express();
+app.get('/index.htm', function(request, response){
+    response.sendfile('yourhtmlpagename.html');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
+
 });
