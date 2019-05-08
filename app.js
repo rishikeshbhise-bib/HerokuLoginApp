5b3aa842c6e3ab4e4c852bd92bd1e400c67e4a95
rishikeshbhise-bib <50315322+rishikeshbhise-bib@users.noreply.github.com>
2019-05-08 12:07:51 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const port=process.env.PORT || 3000
 const server = http.createServer((req, res) => {
 res.statusCode = 200;
 res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+res.end('<!DOCTYPE html><html><body><h2>Apostrophe Inline Login Demo</h2><button type="button" onclick="alert('Hello world!')">Login</button></body></html>');
 });
 server.listen(port,() => {
 console.log(`Server running at port `+port);
